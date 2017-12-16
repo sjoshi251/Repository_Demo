419c1964ffe7a5e4adbb4dd83c8ad189bcb92c75
Asus <Asus@ADMINRG-78O8Q1N>
2017-12-16 14:59:43 +0530

    commit: added - start statement

---
+++ Maven_Eclipse_TestNG_GIT_Jenkins_Integration/src/test/java/com/demoPackage/Demo1.java
@@ -0,0 +1,14 @@
+package com.demoPackage;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+	@Test
+	public void testDemo1(){
+		
+		System.out.println("Start");
+		
+		System.out.println("Maven : Eclipse: TestNG : GIT : JENKINS");
+	}
+
+}
